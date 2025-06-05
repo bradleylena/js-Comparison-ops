d2968b4685576948582ca433da912383c3b414ae
bradleylena <kabakabrad@gmail.com>
2025-06-05 20:40:36 +0300

    changes

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ let canvote = age >= 18 ? "Yes" : "No";
 console.log(canvote);
 
 console.log(10 > 5 ? "Greater" : "Smaller");
+
+let a = 4;
+let b = 9;
+let c = a > b;
+console.log(c);
+
+let firstname = "Brad";
+let secondname = "Lenaiyarra";
+let fullname = firstname + "" + secondname;
+console.log(fullname);
+
+const brad = "brad";
+const lena = 19;
+const finish = brad + lena;
+console.log(finish);
