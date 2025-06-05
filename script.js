0800bc7d010d2dfc2b73aff8967f1149e4e8cdd9
bradleylena <kabakabrad@gmail.com>
2025-06-05 20:18:32 +0300

    js ops

---
+++ script.js
@@ -0,0 +1,18 @@
+let score = 85;
+let grade =
+  score >= 90
+    ? "A"
+    : score >= 80
+    ? "B"
+    : score >= 70
+    ? "C"
+    : score >= 60
+    ? "C"
+    : "end";
+console.log(grade);
+
+let age = 19;
+let canvote = age >= 18 ? "Yes" : "No";
+console.log(canvote);
+
+console.log(10 > 5 ? "Greater" : "Smaller");
